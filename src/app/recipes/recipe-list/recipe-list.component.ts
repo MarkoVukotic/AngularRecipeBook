647b0ec71865900964156d0880b9a08b0f851b74
marko.v <markovukotic32@gmail.com>
2023-05-05 07:00:27 +0200

    Working on a recipe book with Angular

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {Recipe} from "../recipe.module";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+recipes: Recipe[] = [
+  new Recipe('A test recipe', 'This is simply a test desc', 'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1920,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
+];
+
+
+}
